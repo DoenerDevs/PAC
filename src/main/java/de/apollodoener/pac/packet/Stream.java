0b9ff1baede675e0e978256c8eeeaf85abd8f5a5
raffael <tigifan21@gmail.com>
2016-11-05 22:12:39 +0100

    adding Stream class

---
+++ src/main/java/de/apollodoener/pac/packet/Stream.java
@@ -0,0 +1,14 @@
+package de.apollodoener.pac.packet;
+
+/**
+ * Created by raffael on 05.11.16.
+ *
+ * @author Tigifan
+ * @version 1.0
+ */
+public class Stream {
+
+    public void foo() {
+        //Now Nitwel paste here his code
+    }
+}
